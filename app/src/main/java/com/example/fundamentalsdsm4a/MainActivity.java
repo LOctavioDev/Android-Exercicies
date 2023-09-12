9ee267ae325fa3429af86e76435e0a082c418788
LOctavioDev <octaviodevtech@gmail.com>
2023-09-12 15:13:55 -0600

    Firts Android App

--- app/src/main/java/com/example/fundamentalsdsm4a/MainActivity.java
+++ app/src/main/java/com/example/fundamentalsdsm4a/MainActivity.java
@@ -3,22 +3,32 @@ package com.example.fundamentalsdsm4a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etValo1;
-    private EditText etValo2;
+    private EditText etValor1;
+    private EditText etValor2;
     private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etValo1 = findViewById(R.id.etValo1);
-        etValo2 = findViewById(R.id.etValo2);
-        btnAdd.setOnclickListener(new onCreatePanelView().OnClickListener){
+        etValor1 = findViewById(R.id.etValor1);
+        etValor2 = findViewById(R.id.etValor2);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-                    publi
-        }
+            public void onClick(View view) {
+                int n1, n2, res;
+                n1 = Integer.parseInt(etValor1.getText().toString());
+                n2 = Integer.parseInt(etValor2.getText().toString());
+                res = n1 + n2;
+                Toast.makeText(MainActivity.this, "Resultado: " + res, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
